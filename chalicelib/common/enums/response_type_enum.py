5ba0f7a71c05a5f014fe6bd66d3b089209ef1bf9
rafik.gatri <rafik.gatri.2002@gmail.com>
2026-01-09 00:58:07 +0100

    Add users and enterprise projects

--- chalicelib/common/enums/response_type_enum.py
+++
@@ -1,9 +0,0 @@
-"""Response type enumeration"""
-from enum import Enum
-
-
-class ResponseTypeEnum(str, Enum):
-    """Response type enumeration"""
-    SUCCESS = "success"
-    ERROR = "error"
-    WARNING = "warning"
